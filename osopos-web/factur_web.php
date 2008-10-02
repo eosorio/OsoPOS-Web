83ebcec77149bd5c25d9ee307210986b957c841b
linucs <linucs@d5aa04b1-d72a-479a-9c73-8d5326dc8e54>
2008-10-02 18:23:00 +0000

    New class for generation of PDF invoices

--- osopos-web/factur_web.php
+++ osopos-web/factur_web.php
@@ -1,21 +1,21 @@
 <?php  /* -*- mode: php; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 
- Facturación Web 0.4-1. Módulo de facturación de OsoPOS Web.
+ FacturaciÃ³n Web 0.4-1. MÃ³dulo de facturaciÃ³n de OsoPOS Web.
 
-        Copyright (C) 2000,2003-2005 Eduardo Israel Osorio Hernández
+        Copyright (C) 2000,2003-2005 Eduardo Israel Osorio HernÃ¡ndez
 
         Este programa es un software libre; puede usted redistribuirlo y/o
-modificarlo de acuerdo con los términos de la Licencia Pública General GNU
-publicada por la Free Software Foundation: ya sea en la versión 2 de la
-Licencia, o (a su elección) en una versión posterior. 
+modificarlo de acuerdo con los tÃ©rminos de la Licencia PÃºblica General GNU
+publicada por la Free Software Foundation: ya sea en la versiÃ³n 2 de la
+Licencia, o (a su elecciÃ³n) en una versiÃ³n posterior. 
 
-        Este programa es distribuido con la esperanza de que sea útil, pero
-SIN GARANTIA ALGUNA; incluso sin la garantía implícita de COMERCIABILIDAD o
-DE ADECUACION A UN PROPOSITO PARTICULAR. Véase la Licencia Pública General
+        Este programa es distribuido con la esperanza de que sea Ãºtil, pero
+SIN GARANTIA ALGUNA; incluso sin la garantÃ­a implÃ­cita de COMERCIABILIDAD o
+DE ADECUACION A UN PROPOSITO PARTICULAR. VÃ©ase la Licencia PÃºblica General
 GNU para mayores detalles. 
 
-        Debería usted haber recibido una copia de la Licencia Pública General
-GNU junto con este programa; de no ser así, escriba a Free Software
+        DeberÃ­a usted haber recibido una copia de la Licencia PÃºblica General
+GNU junto con este programa; de no ser asÃ­, escriba a Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA. 
 
 */
@@ -25,12 +25,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
   include("include/factur_config.inc");
   include("include/pos-var.inc");
 
-  if (isset($salir)) {
-    include("include/logout.inc");
-  }
-  else {
-    include("include/passwd.inc");
-  }
+  include("include/passwd.inc");
 
   $accion = $_POST['accion'];
   $dia = $_POST['dia'];
@@ -59,7 +54,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
 
 <head>
    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-   <meta name="Author" content="E. Israel Osorio Hernández">
+   <meta name="Author" content="E. Israel Osorio HernÃ¡ndez">
    <title>OsoPOS - FacturWeb v. <? echo $factur_web_vers ?></title>
    <?php include("menu/menu_principal.inc"); ?>
    <link rel="stylesheet" type="text/css" media="screen" href="stylesheets/cuerpo.css">
@@ -73,11 +68,24 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
    </style>
    <script>
    var miPopup
-   function abreVentana(){
-	miPopup = window.open("busca_clientes.php","miwin","width=700,height=400,scrollbars=yes")
+   function abreVentana(scriptFile){
+	miPopup = window.open(scriptFile,"miwin","width=700,height=400,scrollbars=yes")
 	miPopup.focus()
 	}
    </script>
+<?php
+  include("include/pos.inc");
+
+    /*  if (strlen(strstr($accion, "imprimir")) && lee_config($conn, "FACTURA_TIPO") == "PDF") {
+
+    $TMP_DIR = lee_config($conn, "TMP_DIR");
+    $FACTURA_FORMATO_PDF = lee_config($conn, "FACTURA_FORMATO_PDF");
+    $nm_tmpar = tempnam($TMP_DIR, "factura");
+    include("$FACTURA_FORMATO_PDF");
+    echo "<script>window.open(\"leepdf.php?ar=$nm_tmpar\", \"mipdf\", \"width=700,height=500,scrollbars=yes\");</script>";
+      //      unlink($nm_tmpar);
+      }*/
+?>
 </head>
 
 <body>
@@ -105,7 +113,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
 
     /******************** fase dos ******************/
         if ($fase == 1) {
-          include("include/pos.inc");
+          //          include("include/pos.inc");
 
           if (!empty($id_venta)) {
 
@@ -116,7 +124,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
                 if (!isset($existe_venta))
                   $existe_venta = 0;
                 if (!is_array($articulo = lee_venta($id_venta))) {
-                  echo "<div class=\"error_nf\">Error al leer artículos de la venta $id_venta</div><br>\n";
+                  echo "<div class=\"error_nf\">Error al leer artÃ­culos de la venta $id_venta</div><br>\n";
                   $accion = "articulos";
                 }
                 else {
@@ -145,7 +153,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
       include ("include/minegocio_factur_const.inc");
       $articulo = array(new articulosClass);
 
-      /* Los datos de artículos provienen de una forma */
+      /* Los datos de artÃ­culos provienen de una forma */
       if (isset($_POST['desc'])) {
         $es_forma = TRUE;
 
@@ -155,7 +163,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
         $num_arts = 0;
 
         $i = 0;
-        /* Se cuentan cuantos artículos se introdujeron */
+        /* Se cuentan cuantos artÃ­culos se introdujeron */
         //        for( ; strlen($_POST['desc'][$num_arts]) && strlen ($_POST['pu'][$num_arts] && strlen($_POST['cant'][$num_arts]; $num_arts++)
         while (list ($item, $valor) = each ($_POST['desc']))
         {
@@ -204,7 +212,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
         $mes = $mes_a[0]+1;
       }
 
-      /* inicialización de variables */
+      /* inicializaciÃ³n de variables */
       $iva = 0.0; $subtotal = 0.0;
       $impuesto = array();
       for ($z=0; $z<$MAXTAX; $z++, $impuesto[$z] = 0.0);
@@ -232,12 +240,12 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
       $id_cliente = $_POST['id_cliente'];
     }
     else {
-      die("<div class=\"error_f\">Error al obtener datos de artículos</div>\n");
+      die("<div class=\"error_f\">Error al obtener datos de artÃ­culos</div>\n");
     }
 
     $peticion = "INSERT INTO facturas_ingresos VALUES (";
     $peticion.= sprintf("%d, '%d-%d-%d', ", $id, $anio, $mes, $dia) . "'$rfc', '$dom_calle', ";
-    $peticion.= sprintf("%d, '%s', ", $dom_ext, $dom_int);
+    $peticion.= sprintf("'%s', '%s', ", $dom_ext, $dom_int);
     $peticion.= "'$dom_col', '$dom_ciudad', '$dom_edo', ";
     $peticion.= sprintf("%d, %.2f, %.2f", $dom_cp, $subtotal, $iva);
     for ($j=0; $j<$MAXTAX; $j++)
@@ -251,7 +259,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
       die($mens);
     }
     else
-      echo "<center><i>Factura $id, $rfc agregada</i></center><br>";
+      echo "<div class=\"mens_inf\">Factura $id, $rfc agregada</div><br>";
 
     for ($i=0; $i<count($desc); $i++) {
       $peticion = "INSERT INTO fact_ingresos_detalle ";
@@ -273,21 +281,75 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
       if (db_num_rows($resultado) == 0) {
         $peticion = "INSERT INTO clientes_fiscales (\"rfc\", \"curp\", \"nombre\") ";
         $peticion.= " VALUES ('$rfc', '$curp', '$razon_soc')";
-        $mensaje = "<center><i>Cliente $razon_soc agregado</i></center><br>";
+        $mensaje = "<div class=\"mens_inf\">Cliente fiscal $razon_soc agregado</div><br>";
       }
       else
         $mensaje = "";
       if (!$resultado = db_query($peticion, $conn)) {
-        echo "Error al ejecutar $peticion<br>" . db_errormsg($conn) . "</body></html>\n";
+        echo "Error al registrar cliente fiscal<br>" . db_errormsg($conn) . "</body></html>\n";
         exit();
       }
       echo $mensaje;
+
+      $peticion = "SELECT rfc FROM clientes WHERE rfc='$rfc' AND nombres='$razon_soc'";
+      if (!$resultado = db_query($peticion, $conn))
+        die("<div class=\"error_f\">Error al consultar RFC</div>\n");
+
+      if (db_num_rows($resultado) == 0) {
+        $peticion = "INSERT INTO clientes (\"rfc\", \"curp\", \"nombres\", ap_paterno, ap_materno) ";
+        $peticion.= " VALUES ('$rfc', '$curp', '$razon_soc', '', '')";
+        $mensaje = "<div class=\"mens_inf\">Cliente $razon_soc agregado</div><br>\n";
+      }
+      else
+        $mensaje = "";
+      if (!$resultado = db_query($peticion, $conn)) {
+        echo "Error al registrar cliente fiscal<br>" . db_errormsg($conn) . "</body></html>\n";
+        exit();
+      }
+      else {
+        $peticion = "SELECT id FROM clientes WHERE rfc='$rfc' AND curp='$curp' AND nombres='$razon_soc' ORDER BY id DESC";
+        if (!$resultado = db_query($peticion, $conn))
+          echo "<div class=\"error_nf\">Error al consultar id de cliente $razon_soc</div>\n";
+        else {
+          $id_cliente = db_result($resultado, 0, 0);
+          echo $mensaje;
+
+          $peticion = "INSERT INTO domicilios (id_cliente, dom_nombre, dom_calle, dom_numero, dom_inter, dom_col, dom_mpo, dom_ciudad, dom_edo_id, dom_cp) VALUES (";
+          $peticion.= sprintf("%d, 'Fiscal', '%s', '%s', '%s', '%s', '%s', '%s', %d, %d) ",
+                              $id_cliente, $dom_calle,  $dom_ext, $dom_int, $dom_col, $dom_mpo, $dom_ciudad, getDomicilioEstadosID($conn, $dom_edo),  $dom_cp);
+          if (!$resultado = db_query($peticion, $conn))
+            echo "<div class=\"error_nf\">Error al insertar domicilio fiscal de cliente $razon_soc</div>\n$peticion"; 
+          else {
+            $peticion = "SELECT id FROM domicilios WHERE id_cliente=$id_cliente AND dom_nombre='Fiscal' ORDER BY id DESC";
+            if (!$resultado = db_query($peticion, $conn))
+              echo "<div class=\"error_nf\">Error al consultar domicilio fiscal de cliente $razon_soc</div>\n";
+            else {
+              $id_dom_fiscal = db_result($resultado, 0, 0);
+              $peticion = "UPDATE clientes SET dom_principal=$id_dom_fiscal WHERE id=$id_cliente";
+              if (!$resultado = db_query($peticion, $conn))
+                echo "<div class=\"error_nf\">Error al registrar domicilio de cliente $razon_soc</div>\n";
+            }
+          }
+        }
+      }
     }
     $fase = 0;
   }
 
   if ( strlen(strstr($accion, "imprimir")) ) {
-    include("include/pos.inc");
+
+    //    include("include/pos.inc");
+    if (lee_config($conn, "FACTURA_TIPO") == "PDF") {
+      /*      printf("<a href=\"%s?id=%d\" target=\"_top\"><img src=\"imagenes/pdf.png\" border=0></a>",
+       $_SERVER['PHP_SELF], $_POST['id']); */
+      //      $TMP_DIR = lee_config($conn, "TMP_DIR");
+      //      $FACTURA_FORMATO_PDF = lee_config($conn, "FACTURA_FORMATO_PDF");
+      //      $nm_tmpar = tempnam($TMP_DIR, "factura");
+      //      include("$FACTURA_FORMATO_PDF");
+      //      echo "<script>window.open(\"leepdf.php?ar=$nm_tmpar\", \"mipdf\", \"width=700,height=500,scrollbars=yes\");</script>";
+      //      unlink($nm_tmpar);                                                                                                                            
+    }
+ 
     include("include/minegocio.inc");
     include("include/minegocio_factur_const.inc"); 
 
@@ -325,11 +387,14 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
 
     $nm_archivo = "";
 
-    if ($FACTURA_TIPO == "PDF") {
-      $nm_tmpar = tempnam("/tmp", "factura");
-      include("fpdf/personalizado/factura_htech.php");
-      echo "<script>window.open(\"leepdf.php?ar=$nm_tmpar\", \"mipdf\", \"width=700,height=500,scrollbars=yes\");</script>";
+    if (lee_config($conn, "FACTURA_TIPO") == "PDF") {
+      $TMP_DIR = lee_config($conn, "TMP_DIR");
+      $FACTURA_FORMATO_PDF = lee_config($conn, "FACTURA_FORMATO_PDF");
+      $nm_tmpar = tempnam($TMP_DIR, "factura");
+      //      include("$FACTURA_FORMATO_PDF");
+      //echo "<script>window.open(\"leepdf.php?ar=$nm_tmpar\", \"mipdf\", \"width=700,height=500,scrollbars=yes\");</script>";
       //      unlink($nm_tmpar);
+      printf("<script>window.open(\"%s?id=%d\")</script>\n", $FACTURA_FORMATO_PDF, $_POST['id']);
     }
     else {
       $cola_factur = lee_config($conn, "COLA_FACTUR");
@@ -345,7 +410,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
         echo "<div class=\"error_nf\">Error al ejecutar <i>$cmd_impresion $nm_archivo</i></div><br>\n";
       }
       else {
-        echo "<center><i>Factura impresa.</i></center>\n";
+        echo "<div class=\"mens_inf\">Factura impresa.</div>\n";
         fputs($impresion, $imp_buff);
         pclose($impresion);
       }
@@ -403,7 +468,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
     if (empty($id)) {
       $query = "SELECT max(id) AS next_id FROM facturas_ingresos";
       if (!$result = db_query($query, $conn)) {
-        echo "Error al ejecutar $peticion<br>No se pudo extraer último folio<br>" . db_errormsg($conn) . "<br>\n";
+        echo "Error al ejecutar $peticion<br>No se pudo extraer Ãºltimo folio<br>" . db_errormsg($conn) . "<br>\n";
       }
       else {
         $id = db_result($result, 0, "next_id");
@@ -433,9 +498,9 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA02139, USA.
 
 /*
 Pendientes:
-- Verificar que no se excedan los renglones para artículos cuando
-  se usa mas de un renglón para un solo artículo y se usan
-  $ART_MAXRENS artículos
+- Verificar que no se excedan los renglones para artÃ­culos cuando
+  se usa mas de un renglÃ³n para un solo artÃ­culo y se usan
+  $ART_MAXRENS artÃ­culos
 - Hacer captura de observaciones, con su respectivo wrapping
 */
 
